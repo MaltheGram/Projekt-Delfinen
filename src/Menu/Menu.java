ab0a659aa9bbe3a3d179d3a18158e32013d395b0
MaltheGram <malthegram22@gmail.com>
2021-05-18 15:37:48 +0200

    Added basics menu to navigate.

---
+++ src/Menu/Menu.java
@@ -0,0 +1,59 @@
+/*
+ * @author Malthe
+ * 18/05/2021 14.50
+ *
+ * DAT21V2-Projekt-Delfinen
+ *
+ */
+
+/*
+ * @author Malthe
+ * 18/05/2021 14.45
+ *
+ * DAT21V2-Projekt-Delfinen
+ *
+ */
+
+package Menu;
+
+import java.io.PrintStream;
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Menu {
+
+    public static void runMenu() {
+
+    boolean isRunning = true;
+    Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Welcome to \"Delfinen\" system menu\n" +
+                "1: See president options.\n" +
+                "2: See accountant options.\n" +
+                "3: See coach options\n" +
+                "9: Exit");
+
+        int input = sc.nextInt();
+        //while (isRunning)
+        if (input == 1){
+            // TODO: SEE MEMBER OPTIONS
+            PresidentMenu.runPresidentMenu();
+        }
+        if (input == 2){
+            // TODO: SEE ACCOUNTANT OPTIONS
+            AccountantMenu.runAccountantMenu();
+        }
+        if (input == 3){
+            // TODO: SEE COACH OPTIONS
+            CoachMenu.runCoachMenu();
+        }
+        else if (input == 9)
+            System.out.println("Terminating.....");
+            isRunning = false;
+
+
+
+    }
+}
