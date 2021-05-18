c3d3bd461049461e77ad5cb8cc86cde7a3bda631
MaltheGram <malthegram22@gmail.com>
2021-05-18 11:53:15 +0200

    commit

--- src/Member/Member.java
+++ src/Member/Member.java
@@ -4,16 +4,16 @@ public class Member {
 
     private String birthDate;
     private String name;
-    private String phoneNumber;
+    private String address;
     private int memberId;
-    private boolean activeMember;
+    private boolean isActiveMember;
 
 
     public Member(String name, String birthDate, String phoneNumber, boolean activeMember, int memberId){
         this.name = name;
         this.birthDate = birthDate;
-        this.phoneNumber = phoneNumber;
-        this.activeMember = activeMember;
+        this.address = phoneNumber;
+        this.isActiveMember = activeMember;
         this.memberId = memberId;
     }
 
@@ -33,12 +33,12 @@ public class Member {
         this.name = name;
     }
 
-    public String getPhoneNumber() {
-        return phoneNumber;
+    public String getAddress() {
+        return address;
     }
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
+    public void setAddress(String address) {
+        this.address = address;
     }
 
     public int getMemberId() {
@@ -50,11 +50,11 @@ public class Member {
     }
 
     public boolean isActiveMember() {
-        return activeMember;
+        return isActiveMember;
     }
 
     public void setActiveMember(boolean activeMember) {
-        this.activeMember = activeMember;
+        this.isActiveMember = activeMember;
     }
 
 }
