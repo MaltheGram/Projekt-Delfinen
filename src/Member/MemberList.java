998137979be10a6c8da00db23c7d64d221d6b530
Simon Gredal <simon.gredal@icloud.com>
2021-05-18 13:01:34 +0200

    Add membership files

---
+++ src/Member/MemberList.java
@@ -0,0 +1,22 @@
+package Member;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemberList {
+
+    private static final Map<Integer, Member> members = new HashMap<>();
+
+    public static Collection<Member> getAllMembers() {
+        return Collections.unmodifiableCollection( members.values() );
+    }
+
+
+    public static Collection<Member> getOverdueMembers() {
+        return Collections.unmodifiableCollection(
+                members.values().stream().filter(member -> member.getMembership.isOverdue()
+        );
+    }
+}
---
+++ src/Membership/Membership.java
@@ -0,0 +1,16 @@
+package Membership;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Membership {
+    private final List<MembershipFee> membershipFees;
+
+    public Membership() {
+         this.membershipFees = new ArrayList<>();
+    }
+
+    public Boolean isOverdue() {
+        return membershipFees.stream().anyMatch( membershipFee -> membershipFee.isOverdue() );
+    }
+}
