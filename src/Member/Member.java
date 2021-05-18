2e477087c3bd7071aa3bf0e40436d33f25cd3484
MaltheGram <malthegram22@gmail.com>
2021-05-18 13:05:15 +0200

    added random generator for MemberId

--- src/Member/Member.java
+++ src/Member/Member.java
@@ -1,20 +1,34 @@
 package Member;
 
+import Service.MemberIdGenerator;
+
 public class Member {
 
     private String birthDate;
     private String name;
     private String address;
-    private int memberId;
+    private String phoneNumber;
     private boolean isActiveMember;
+    private MemberIdGenerator memberIdGenerator;
 
 
-    public Member(String name, String birthDate, String phoneNumber, boolean isActiveMember, int memberId){
+
+    public Member(String name, String birthDate, String address, String phoneNumber, boolean isActiveMember, MemberIdGenerator memberIdGenerator){
         this.name = name;
         this.birthDate = birthDate;
-        this.address = phoneNumber;
+        this.address = address;
         this.isActiveMember = isActiveMember;
-        this.memberId = memberId;
+        this.phoneNumber = phoneNumber;
+        this.memberIdGenerator = memberIdGenerator;
+    }
+
+
+    public MemberIdGenerator getMemberIdGenerator() {
+        return memberIdGenerator;
+    }
+
+    public void setMemberIdGenerator(MemberIdGenerator memberIdGenerator) {
+        this.memberIdGenerator = memberIdGenerator;
     }
 
     public String getBirthDate() {
@@ -41,19 +55,24 @@ public class Member {
         this.address = address;
     }
 
-    public int getMemberId() {
-        return memberId;
-    }
-
-    public void setMemberId(int memberId) {
-        this.memberId = memberId;
-    }
-
     public boolean isActiveMember() {
         return isActiveMember;
     }
 
     public void setActiveMember(boolean isActiveMember) {
         this.isActiveMember = isActiveMember;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Member data:\n" + "BirthDate: " + birthDate + "\nName: " + name + "\nAddress: " + address + "\nMember id: " + memberIdGenerator + "\nActive member: " + isActiveMember;
     }
 }
