e444d2dfa8ba0b4ac6b24d5bde702c72cae578ec
Mark Friis Larsen <marklarsen96@gmail.com>
2021-05-19 11:59:55 +0200

    fileHandler update

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -1,3 +1,10 @@
+/*
+ * @author Mark "Massive Legend" Larsen
+ * 19/05/2021 11.24
+ *
+ * DAT21V2-Projekt-Delfinen
+ *
+ */
 import Member.Member;
 import Service.MemberIdGenerator;
 
@@ -16,13 +23,24 @@ public class FileHandler {
                 p.println(m.getKey()+"="+m.getValue());
             }
 
+            //VERSION 2.0
+            /*
+            FileOutputStream write = new FileOutputStream(new File(filename + ".txt"));
+            ObjectOutputStream o = new ObjectOutputStream(f);
+
+            //Write in format
+            for(Map.Entry<Integer,Member> m :map.entrySet()){
+                o.writeObject(m)
+            }
+             */
+
             //Cleanup
             p.flush();
             p.close();
             write.close();
         } catch(Exception e) {}
     }
-    public static HashMap<Integer,Member> readFile(File filename){
+    public static HashMap<Integer,Member> readFile(String filename){
         try {
             File read = new File(filename + ".txt");
             FileInputStream f = new FileInputStream(read);
@@ -33,9 +51,7 @@ public class FileHandler {
             String currentLine;
             while(sc.hasNextLine()) {
                 currentLine= sc.nextLine();
-                //LocalDate MarkDate = LocalDate.of(1996, 10, 30);
                 MemberIdGenerator memberIdGenerator2 = new MemberIdGenerator();
-                //Member Mark = new Member("Mark", MarkDate, "Lygten 37", "8888888", true, memberIdGenerator2);
 
                 //GET KEY AND USERDATA
                 String[] member = currentLine.split("=");
@@ -44,19 +60,39 @@ public class FileHandler {
                 String name = memberData[0];
                 String birthDate = memberData[1];
                 String address = memberData[2];
-                String isActive = memberData[3];
-                String membership = memberData[3];
+                boolean isActive = false;
+                String membership = memberData[4];
                 String phone = memberData[5];
                 String ID = memberData[6];
 
-                //String birthDate[] = birth.split["-"];
-                LocalDate birthFix = LocalDate.of(9999, 1, 30);
+                //RESTRUCTURE DATA TO FIT MEMBER-CONSTRUCTOR
+                String birthDateSplit[] = birthDate.split("-");
+                LocalDate birthFix = LocalDate.of(Integer.parseInt(birthDateSplit[0]), Integer.parseInt(birthDateSplit[1]), Integer.parseInt(birthDateSplit[2]));
 
-                Member tmpMember = new Member(name,birthFix,address,phone,false,memberIdGenerator2);
-                mapInFile.put(Integer.parseInt(member[0]),tmpMember);
+                //if (memberData[3]=="true") {isActive = true;}
 
+                //Member tmpMember = new Member(name,birthFix,address,phone,isActive,ID);
+                //mapInFile.put(Integer.parseInt(member[0]),tmpMember);
+
+                //Cleanup
+                f.close();
+                sc.close();
             }
 
+            //VERSION 2.0
+            /*
+            FileInputStream fi = new FileInputStream(new File("myObjects.txt"));
+            ObjectInputStream oi = new ObjectInputStream(fi);
+
+            boolean flag = true;
+            while (flag) {
+            try {
+                Member tmpMember = (Member) oi.readObject();
+                mapInFile.put(tmpMember.getID,tmpMember);
+            } catch (Exception e) {
+                flag = false;
+            }
+            */
             System.out.println(mapInFile);
             return mapInFile;
 
