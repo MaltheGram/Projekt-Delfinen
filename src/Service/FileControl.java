481cea0cf0105e417ecc5c7a62f842a6fd78f4c2
MaltheGram <malthegram22@gmail.com>
2021-05-20 12:39:19 +0200

    commits

--- src/Service/FileControl.java
+++ src/Service/FileControl.java
@@ -1,4 +1,12 @@
 /*
+ * @author Malthe
+ * 20/05/2021 12.29
+ *
+ * DAT21V2-Projekt-Delfinen
+ *
+ */
+
+package Service;/*
  * @author Mark "Massive Legend" Larsen
  * 19/05/2021 11.24
  *
@@ -7,13 +15,13 @@
  */
 import Member.Member;
 import Service.MemberIdGenerator;
-
+import Member.MemberList;
 import java.io.*;
 import java.time.LocalDate;
 import java.util.*;
 
 public class FileControl {
-    public static void writeObjectToFile(HashMap<String,Member> serializableObject, String filename){
+    public static void writeObjectToFile(MemberList serializableObject, String filename){
         try {
             //WRITE FILE
             FileOutputStream write = new FileOutputStream(new File(filename + ".ser"));
@@ -29,13 +37,13 @@ public class FileControl {
             System.out.println(e);
         }
     }
-    public static HashMap<String,Member> readMemberListfromFile(String filename){
-        HashMap<String,Member> mapInFile = new HashMap();
+    public static MemberList readMemberListfromFile(String filename){
+        MemberList mapInFile = new MemberList();
         try {
             //READ FILE
             FileInputStream f = new FileInputStream(new File(filename + ".ser"));
             ObjectInputStream in = new ObjectInputStream(f);
-            mapInFile = (HashMap<String,Member>) in.readObject();
+            mapInFile = (MemberList)in.readObject();
 
             //CLEAN UP
             f.close();
