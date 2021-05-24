b1e0538d7707cc0cb7944def5571e8d0bec87171
MaltheGram <malthegram22@gmail.com>
2021-05-24 13:10:07 +0200

    changed Result to normal class

--- src/Competition/Result.java
+++ src/Competition/Result.java
@@ -7,18 +7,78 @@ package Competition;
 
 import Group.Discipline;
 import Member.Member;
-import Service.UserInput;
 
+import java.io.Serializable;
 import java.time.*;
 
-record Result( Member memberID,
+public class Result implements Serializable {
+    private Member memberId;
+    private LocalDate date;
+    private ResultType type;
+    private Discipline discipline;
+    private Duration time;
+
+    public Result(Member memberId, LocalDate date, ResultType type, Discipline discipline, Duration time) {
+        this.memberId = memberId;
+        this.date = date;
+        this.type = type;
+        this.discipline = discipline;
+        this.time = time;
+    }
+
+    public Member getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Member memberId) {
+        this.memberId = memberId;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public ResultType getType() {
+        return type;
+    }
+
+    public void setType(ResultType type) {
+        this.type = type;
+    }
+
+    public Discipline getDiscipline() {
+        return discipline;
+    }
+
+    public void setDiscipline(Discipline discipline) {
+        this.discipline = discipline;
+    }
+
+    public Duration getTime() {
+        return time;
+    }
+
+    public void setTime(Duration time) {
+        this.time = time;
+    }
+}
+
+
+
+/*record Result( Member memberID,
                LocalDate date,
                ResultType type,
                Discipline discipline,
                Duration time
-) {
+) implements Serializable {
 
-}
+ */
+
+
 
 
 
