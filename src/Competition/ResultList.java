97fa2160b9018564d41b468cbc44d5b48b096231
Simon Gredal <simon.gredal@icloud.com>
2021-05-24 12:14:16 +0200

    Add resultlist

---
+++ src/Competition/ResultList.java
@@ -0,0 +1,47 @@
+package Competition;
+
+import Member.Member;
+import Service.FileControl;
+import java.util.*;
+
+public class ResultList {
+
+    private final String filePath;
+    private final HashMap<String, List<Result>> results;
+
+    public ResultList(String filePath) {
+        this.filePath = filePath;
+        this.results = FileControl.readSerializableFromFile(this.filePath, new HashMap<>() );
+    }
+
+    public void addNewResult(Member member, Result result) {
+        if ( this.results.containsKey(member.getMemberId()) ) {
+            this.results.get(member.getMemberId()).add(result);
+        } else {
+            ArrayList<Result> list = new ArrayList<>();
+            list.add(result);
+            this.results.put(member.getMemberId(), list );
+        }
+
+        FileControl.writeSerializableToFile(this.results, this.filePath);
+    }
+
+    public void removeResult(String memberID) {
+        // does nothing
+    }
+
+    public List<Result> getResultsByID(String memberID) {
+        return this.results.getOrDefault(memberID, new ArrayList<Result>());
+    }
+
+    public Collection<List<Result>> getAllResults() {
+        return Collections.unmodifiableCollection(
+                this.results.values()
+        );
+    }
+
+    @Override public String toString() {
+        return this.results.toString();
+    }
+
+}
