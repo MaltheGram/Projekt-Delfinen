761d8cf04e072ef9372ec1c81810b6249f695731
MaltheGram <malthegram22@gmail.com>
2021-05-25 22:28:58 +0200

    adding TeamList.java

---
+++ src/Competition/TeamList.java
@@ -0,0 +1,51 @@
+/*
+ * @author Malthe
+ * 25/05/2021 21.11
+ *
+ * DAT21V2-Projekt-Delfinen
+ *
+ */
+
+package Competition;
+
+
+import Member.Member;
+import Service.FileControl;
+import Service.UserInput;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Set;
+
+public class TeamList {
+
+    private final String filePath;
+    private final HashMap<String, Team> teams;
+
+    public TeamList(String filePath){
+        this.filePath = filePath;
+        this.teams = FileControl.readSerializableFromFile(this.filePath, new HashMap<>());
+    }
+
+    public void addNewTeam(Team team){
+        this.teams.put(team.getName(), team);
+        FileControl.writeSerializableToFile(this.teams,this.filePath);
+    }
+
+    public Set<String> getTeams(){
+         return teams.keySet();
+    }
+
+    // Refers to UserInput.java line 266
+    public Collection<Team> getAllTeams(){
+        return Collections.unmodifiableCollection(
+                this.teams.values()
+        );
+    }
+
+    @Override
+    public String toString(){
+        return teams.toString();
+    }
+}
